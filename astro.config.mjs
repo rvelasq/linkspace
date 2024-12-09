3501ca4b3561a82768f322f396cc6e1ed7b1b3d5
Blazej Adamczyk <sein.adamczyk@gmail.com>
2024-12-09 22:04:28 +0100

    build fixes

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,10 +3,14 @@ import { defineConfig } from 'astro/config';
 
 import tailwind from '@astrojs/tailwind';
 import icon from "astro-icon";
+import yaml from '@rollup/plugin-yaml';
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind(), icon({
     iconDir: "src/assets/icons",
   })],
+  vite: {
+    plugins: [yaml()]
+  }
 });
--- src/libs/config.ts
+++ src/libs/config.ts
@@ -3,12 +3,16 @@ import { readFileSync } from 'fs';
 import { fileURLToPath } from 'url';
 import { dirname, join } from 'path';
 import type { LinksConfig } from '../types/config';
+import yml from '../config/links.yaml';
 
-
-export function getConfig(): LinksConfig {
+export function getDynamicConfig(): LinksConfig {
     const __filename = fileURLToPath(import.meta.url);
     const __dirname = dirname(__filename);
     const configPath = join(__dirname, '../config/links.yaml');
     const configFile = readFileSync(configPath, 'utf8');
     return yaml.load(configFile) as LinksConfig;
+}
+
+export function getConfig(): LinksConfig {
+    return yml as LinksConfig;
 }
